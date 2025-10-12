5a9bf33cdb0f00ada771991b65c3bf335748becf
Dor Varsulker <dvarsul@gmail.com>
2025-10-12 14:41:47 +0300

    experimenting with square.cpp

--- Chapter-2/2.1-Square/square.cpp
+++ Chapter-2/2.1-Square/square.cpp
@@ -5,9 +5,8 @@
 void drawScene(void) {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3f(0.0, 0.0, 0.0);
-
   // Draw a polygon with specified vertices.
+  glColor3f(1.0, 0.0, 0.0);
   glBegin(GL_POLYGON);
   glVertex3f(20.0, 20.0, 0.0);
   glVertex3f(80.0, 20.0, 0.0);
@@ -16,9 +15,13 @@ void drawScene(void) {
   glEnd();
 
   glBegin(GL_POLYGON);
+  glColor3f(1.0, 0.0, 0.0);
   glVertex3f(120.0, 120.0, 0.0);
+  glColor3f(0.0, 1.0, 0.0);
   glVertex3f(120.0, 180.0, 0.0);
+  glColor3f(0.0, 0.0, 1.0);
   glVertex3f(180.0, 180.0, 0.0);
+  glColor3f(1.0, 1.0, 0.0);
   glVertex3f(180.0, 120.0, 0.0);
   glEnd();
 
