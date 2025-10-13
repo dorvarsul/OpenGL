d1431f14ff0860b68bb53e0702755cf5169f6af2
Dor Varsulker <dvarsul@gmail.com>
2025-10-13 16:05:46 +0300

    added leaf excercise

--- Book/Chapter-2/2.3-Parabola/parabola.cpp
+++ Book/Chapter-2/2.3-Parabola/parabola.cpp
@@ -1,17 +1,26 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+static int M = 3;
+
 // Drawing routine.
 void drawScene(void) {
+  int i;
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3f(1.0, 0.0, 0.0);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-  glBegin(GL_POLYGON);
-  glVertex3f(80.0, 20.0, 0.0);
-  glVertex3f(40.0, 40.0, 0.0);
-  glVertex3f(20.0, 80.0, 0.0);
-  glVertex3f(20.0, 20.0, 0.0);
+  glColor3f(0.0, 0.0, 0.0);
+
+  // The 2M+1 vertices on the parabola are equally spaced in x-coordinates.
+  // The parabola is translated and scaled to center it in the window.
+  glBegin(GL_LINE_STRIP);
+  for (i = -M; i <= M; ++i) {
+    glVertex3f(50.0 + 50.0 * (float)i / M, 100.0 * (float)(i * i) / (M * M),
+               0.0);
+  }
   glEnd();
 
   glFlush();
@@ -23,13 +32,9 @@ void setup(void) { glClearColor(1.0, 1.0, 1.0, 0.0); }
 // OpenGL window reshape routine.
 void resize(int w, int h) {
   glViewport(0, 0, w, h);
-
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  // Set up virtual viewerbox where the programmer can draw scenes
-  // --glOrtho(Left, Right, Bottom, Top, Near, Far)
   glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
-
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 }
@@ -40,25 +45,40 @@ void keyInput(unsigned char key, int x, int y) {
   case 27:
     exit(0);
     break;
+  case '+':
+    M++;
+    glutPostRedisplay();
+    break;
+  case '-':
+    if (M > 2)
+      M--;
+    glutPostRedisplay();
+    break;
   default:
     break;
   }
 }
 
+// Routine to output interaction instructions to the C++ window.
+void printInteraction(void) {
+  std::cout << "Interaction:" << std::endl;
+  std::cout << "Press +/- to increase/decrease the number of vertices on the "
+               "parabola."
+            << std::endl;
+}
+
 // Main routine.
 int main(int argc, char **argv) {
+  printInteraction();
   glutInit(&argc, argv);
 
   glutInitContextVersion(4, 3);
   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-
   glutInitWindowSize(500, 500);
   glutInitWindowPosition(100, 100);
-
-  glutCreateWindow("square.cpp");
-
+  glutCreateWindow("parabola.cpp");
   glutDisplayFunc(drawScene);
   glutReshapeFunc(resize);
   glutKeyboardFunc(keyInput);
